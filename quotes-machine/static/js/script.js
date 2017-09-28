0e82177a48d483d08d92fc9283b57739ba2b93c3
Satoshi Scott Iwako <iwakoscott@gmail.com>
2017-09-28 14:19:12 -0700

    add basic functionality.

---
+++ quotes-machine/static/js/script.js
@@ -0,0 +1,44 @@
+function getRandomColor() {
+     var letters = 'BCDEF'.split('');
+     var color = '#';
+     for (var i = 0; i < 6; i++ ) {
+         color += letters[Math.floor(Math.random() * letters.length)];
+     }
+     return color;
+ }
+
+// Knockout JS
+function QuoteAppViewModel(){
+  var self = this;
+
+  self.quote = ko.observable("");
+  self.author = ko.observable("");
+
+  self.formatAuthor = ko.computed(function(){
+    if (self.author()){
+      return "- " + self.author();
+    } else {
+      return '';
+    }
+  });
+
+  self.formatQuote = ko.computed(function(){
+    return '<h1> <i class="fa fa-quote-left"></i> ' + self.quote() + ' <i class="fa fa-quote-right"></i>' + '</h1>';
+  });
+
+  self.randomQuote = function(){
+    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json){
+      self.quote(json.quoteText);
+      self.author(json.quoteAuthor);
+      $('.container').css({"background-color": getRandomColor()});
+    });
+  }
+
+  $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json){
+    self.quote(json.quoteText);
+    self.author(json.quoteAuthor);
+  })
+
+}
+
+ko.applyBindings(new QuoteAppViewModel());
